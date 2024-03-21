fe317c4f64a8f4b73eddcc7ffadb9c2be34f2967
Shubham <sdawasare1@gmail.comm>
2024-03-21 19:17:21 +0530

    first commit

---
+++ basics/format.py
@@ -0,0 +1,6 @@
+first_name = 'Shubham'
+last_name='awasare'
+
+print('My name is {} {}'.format(first_name,last_name))
+
+print("My name in reverse is {last} {first}".format(last=last_name,first=first_name))
